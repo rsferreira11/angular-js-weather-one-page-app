c7cf4aeb4a528f75bce01351ee6fe4188f1e153a
Rafael Ferreira <rsferreira11@gmail.com>
2016-12-09 16:31:58 +0200

    Fix service error

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:latest
+FROM node:7-slim
 MAINTAINER Rafael de Souza Ferreira <rsferreira11@gmail.com>
 
 # Create app directory
--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -1,27 +1,24 @@
 // CONTROLLERS
 weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService){
-    
+
     $scope.city = cityService.city;
-    
+
     $scope.$watch('city', function(){
         cityService.city = $scope.city;
     });
-    
+
     $scope.submit = function() {
         $location.path("/forecast");
     };
-    
+
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', '$http', function($scope, $routeParams, cityService, weatherService, $http){
-    
+weatherApp.controller('forecastController', ['$scope', '$routeParams', '$http', 'cityService', function($scope, $routeParams, $http, cityService){
+
     $scope.city = cityService.city;
-    
+
     $scope.days = $routeParams.days || '2';
-    
-    //$scope.weatherResult = weatherService.getWeather($scope.city, $scope.days);
-    //console.log($scope);
-    
+
     $http({
         method: 'GET',
         url: "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=1c8c9b71a5ecbea2df472cd257cb8e29",
@@ -31,15 +28,13 @@ weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityServ
         }
     })
     .then(function successCallback(response){
-        console.log(response.data.list);
         $scope.weatherResult = response.data;
     });
-    
+
     $scope.convertToCelsius = function(kelvin) {
-        console.log(kelvin);
         return Math.round(kelvin - 273.15);
     }
-    
+
     $scope.convertToDate = function(dt){
         return new Date(dt * 1000);
     }
